2c85ae29bb4d9a550cc136a93958245457556c1b
zzlmg <zhangzuolumg@163.com>
2026-06-26 23:45:04 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'amblyopia-v4';
+const CACHE = 'amblyopia-v5';
 const FILES = [
   './',
   'index.html',
@@ -24,28 +24,37 @@ const FILES = [
   'assets/icons/icon-maskable-512.png'
 ];
 
-self.addEventListener('install', e => {
+self.addEventListener('install', function(e) {
+  console.log('SW installing...');
   self.skipWaiting();
   e.waitUntil(
-    caches.open(CACHE).then(cache =>
-      Promise.allSettled(FILES.map(url =>
-        fetch(url, { cache: 'no-cache' }).then(r => {
-          if (r.ok) cache.put(url, r);
-        })
-      ))
-    )
+    caches.open(CACHE).then(function(cache) {
+      return Promise.all(FILES.map(function(url) {
+        return fetch(url).then(function(r) {
+          if (r.ok) { cache.put(url, r.clone()); }
+        }).catch(function() {});
+      }));
+    })
   );
 });
 
-self.addEventListener('activate', e => {
+self.addEventListener('activate', function(e) {
+  console.log('SW activating...');
   e.waitUntil(
-    caches.keys().then(keys => Promise.all(
-      keys.filter(k => k !== CACHE).map(k => caches.delete(k))
-    )).then(() => self.clients.claim())
+    caches.keys().then(function(keys) {
+      return Promise.all(keys.filter(function(k) { return k !== CACHE; })
+        .map(function(k) { return caches.delete(k); }));
+    }).then(function() { return self.clients.claim(); })
   );
 });
 
-self.addEventListener('fetch', e => {
-  if (e.request.url.includes('manifest.json')) return;
-  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
+self.addEventListener('fetch', function(e) {
+  if (e.request.method !== 'GET') return;
+  e.respondWith(
+    caches.match(e.request).then(function(r) {
+      return r || fetch(e.request).then(function(nr) {
+        return nr;
+      });
+    })
+  );
 });
